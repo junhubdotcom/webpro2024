bd77725f2d06462df1e993e912a5cefbbbce78c7
LJOEYING <joeyingloh0326@gmail.com>
2024-06-14 05:49:56 +0800

    help

--- message-board/messageBoard.php
+++ message-board/messageBoard.php
@@ -5,12 +5,13 @@
 <title>forum</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<script src="messageBoard.js"></script>
-  <link type="text/css" rel="stylesheet" href="messageBoard.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="messageBoard.js"></script>
+<link type="text/css" rel="stylesheet" href="messageBoard.css">
 </head>
+<body>
 
 <!-- Modal -->
 <div id="ReplyModal" class="modal fade" role="dialog">
@@ -24,61 +25,60 @@
       </div>
       <div class="modal-body">
         <form name="frm1" method="post">
-            <input type="hidden" id="commentid" name="Rcommentid">
-        	<div class="form-group">
-        	  <label for="usr">Write your name:</label>
-        	  <input type="text" class="form-control" name="Rname" required>
-        	</div>
-            <div class="form-group">
-              <label for="comment">Write your reply:</label>
-              <textarea class="form-control" rows="5" name="Rmsg" required></textarea>
-            </div>
-        	 <input type="button" id="btnreply" name="btnreply" class="btn btn-primary" value="Reply">
-      </form>
+          <input type="hidden" id="commentid" name="Rcommentid">
+          <div class="form-group">
+            <label for="usr">Write your name:</label>
+            <input type="text" class="form-control" name="Rname" required>
+          </div>
+          <div class="form-group">
+            <label for="comment">Write your reply:</label>
+            <textarea class="form-control" rows="5" name="Rmsg" required></textarea>
+          </div>
+          <input type="button" id="btnreply" name="btnreply" class="btn btn-primary" value="Reply">
+        </form>
       </div>
     </div>
-
   </div>
 </div>
 
 <div class="d-flex justify-content-between align-items-center">
-  <a href="help.html" class="btn1 btn-primary btn-lg">&lt; Back to Help</a>
+  <a href="../help.html" class="btn1 btn-primary btn-lg">&lt; Back to Help</a>
 </div>
 
-<div class="container">
-<img class="mx-auto d-block" src ="images/oLogo.png" width="300" height="120">
+<div class="container-fluid">
+  <div class="text-center">
+    <img src="../images/oLogo.png" width="300" height="150">
+  </div>
 
-<div class="panel panel-default" style="margin-top:15px">
-  <div class="panel-body">
-    <h3>Community forum</h3>
-    <hr>
-    <form name="frm" method="post" action="register.php">
+  <div class="panel panel-default" style="margin-top:15px">
+    <div class="panel-body">
+      <h3>Community forum</h3>
+      <hr>
+      <form name="frm" method="post" action="register.php">
         <input type="hidden" id="commentid" name="Pcommentid" value="0">
         <input type="hidden" name="redirect_url" value="<?php echo $_SERVER['REQUEST_URI']; ?>">
-	<div class="form-group">
-	  <label for="usr">Write your name:</label>
-	  <input type="text" class="form-control" name="name" required>
-	</div>
-    <div class="form-group">
-      <label for="comment">Write your question:</label>
-      <textarea class="form-control" rows="5" name="msg" required></textarea>
+        <div class="form-group">
+          <label for="usr">Write your name:</label>
+          <input type="text" class="form-control" name="name" required>
+        </div>
+        <div class="form-group">
+          <label for="comment">Write your question:</label>
+          <textarea class="form-control" rows="5" name="msg" required></textarea>
+        </div>
+        <input type="button" id="butsave" name="save" class="btn sendbtn" value="Send">
+      </form>
     </div>
-	 <input type="button" id="butsave" name="save" class="btn btn-primary" value="Send">
-  </form>
   </div>
-</div>
-  
 
-<div class="panel panel-default">
-  <div class="panel-body">
-    <h4>Recent questions</h4>           
-	<table class="table" id="MyTable" style="background-color: #edfafa; border:0px;border-radius:10px">
-	  <tbody id="record">
-		
-	  </tbody>
-	</table>
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <h4>Recent questions</h4>
+      <table class="table" id="MyTable" style="background-color: #edfafa; border:0px;border-radius:10px">
+        <tbody id="record">
+        </tbody>
+      </table>
+    </div>
   </div>
-</div>
 
 </div>
 
