2c9639c988283223f1c0c4ab02ea8797e8466b10
junhubdotcom <furnoyong@gmail.com>
2024-06-22 23:14:25 +0800

    databaseupdate

--- connect.php
+++ connect.php
@@ -4,6 +4,7 @@ $host="localhost";
 $user="root";
 $pass="";
 $db="subprize";
+
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
