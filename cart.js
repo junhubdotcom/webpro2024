83ff13582fb5678a65ed147e4ec78da5a8f5c190
liuqii03 <liuqii0401@gmail.com>
2024-06-25 16:14:42 +0800

    chg

---
+++ cart.js
@@ -0,0 +1,20 @@
+const openBtn = document.getElementById('open_cart_btn');
+const cart = document.getElementById('sidecart');
+const closeBtn = document.getElementById('close_btn');
+const backdrop = document.querySelector('.backdrop');
+
+openBtn.addEventListener('click', openCart);
+closeBtn.addEventListener('click', closeCart);
+backdrop.addEventListener('click', closeCart);
+
+// Open Cart
+function openCart() {
+    cart.classList.add('open');
+    backdrop.classList.add('open');
+}
+
+// Close Cart
+function closeCart() {
+    cart.classList.remove('open');
+    backdrop.classList.remove('open');
+}
