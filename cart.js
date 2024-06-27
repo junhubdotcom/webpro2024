7c4fbc7ec5e3ec553766262d31503cda4b4c5a0b
junhubdotcom <furnoyong@gmail.com>
2024-06-27 21:47:23 +0800

    temp

--- cart.js
+++ cart.js
@@ -1,26 +1,121 @@
-const openBtn = document.getElementById('open_cart_btn');
-const cart = document.getElementById('sidecart');
-const closeBtn = document.getElementById('close_btn');
-const backdrop = document.querySelector('.backdrop');
+const ITEMS = [
+  {
+    id: 1,
+    name: "Japan Suprize Box",
+    price: 30,
+    image: "images/shoppingcart.jpg",
+    monthlyPlan: 12,
+  },
+  {
+    id: 2,
+    name: "Thailand Suprize Box",
+    price: 30,
+    image: "images/shoppingcart.jpg",
+    monthlyPlan: 12,
+  },
+];
 
-openBtn.addEventListener('click', openCart);
-closeBtn.addEventListener('click', closeCart);
-backdrop.addEventListener('click', closeCart);
+
+const cart_data = [
+  
+];
+
+const openBtn = document.getElementById("open_cart_btn");
+const cart = document.getElementById("sidecart");
+const closeBtn = document.getElementById("close_btn");
+const backdrop = document.querySelector(".backdrop");
+const itemsEl = document.querySelector(".items");
+const cartItems = document.querySelector(".cart_items");
+
+openBtn.addEventListener("click", openCart);
+closeBtn.addEventListener("click", closeCart);
+backdrop.addEventListener("click", closeCart);
+
+renderItems();
+renderCartItems();
 
 // Open Cart
 function openCart() {
-    cart.classList.add('open');
-    backdrop.style.display = 'block'
+  cart.classList.add("open");
+  backdrop.style.display = "block";
 
-    setTimeout(() => { backdrop.classList.add('show') }, 0)
+  setTimeout(() => {
+    backdrop.classList.add("show");
+  }, 0);
+}
+
+//Add Item to cart
+function addItem(idx, itemId){
+    //find same items 
+    cart_data.push(ITEMS[idx]);
+    updateCart();
+    openCart();
 }
 
 // Close Cart
 function closeCart() {
-    cart.classList.remove('open');
-    backdrop.classList.remove('show');
+  cart.classList.remove("open");
+  backdrop.classList.remove("show");
 
-    setTimeout(() => {
-        backdrop.style.display = 'none'
-    }, 500)
+  setTimeout(() => {
+    backdrop.style.display = "none";
+  }, 500);
+}
+
+// Render Items
+function renderItems() {
+  ITEMS.forEach((item, idx) => {
+    const itemEl = document.createElement("div")
+    itemEl.classList.add("item")
+    itemEl.onclick = () => addItem(idx, item.id)
+    itemEl.innerHTML = `
+            <img src="${item.image}" alt="${item.name}">
+            <div class="content">
+                <h4>${item.name}</h4>
+                <p>$${item.price}</p>
+                <p>$${item.price}</p>
+                <p>Monthly Plan: ${item.monthlyPlan}</p>
+            </div>
+            <button>Add to Cart</button>
+        `;
+    itemsEl.appendChild(itemEl);
+  });
+}
+
+//Display /Render Cart Items
+function renderCartItems() {
+    //remove everything from cart
+    cartItems.innerHTML = "";
+    //add new data
+    cart_data.forEach((item) => {
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("cart_item");
+    cartItem.innerHTML = `
+            <div class="remove_item">
+            <span>&times;</span>
+                    </div>
+                    <div class="item_img">
+                        <img src="${item.image} "alt="snackbox" />
+                    </div>
+                    <div class="item_details">
+                        <p>${item.name} SUB-Prize Snack Box </p>
+                        <strong>$${item.price}</strong>
+                        <div class="plan">
+                            <select id="city" name="city" class="form-control" required>
+                                <option value="">Select City...</option>
+                                <option value="12">12 Months</option>
+                                <option value="6">6 Months</option>
+                                <option value="3">3 Months</option>
+                                <option value="1">1 Month</option>
+                            </select>
+                        </div>
+                    </div>`;
+
+    cartItems.appendChild(cartItem);
+  });
+}
+
+function updateCart(){
+    //render cart items when cart is updated
+    renderCartItems()
 }
